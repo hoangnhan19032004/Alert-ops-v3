b830644ed38b51a336ca11f2253fcf9b87178e09
Hoang Nhan <phannguyenhoangnhan1903@gmail.com>
2026-05-26 09:10:50 +0700

    Update

--- backend/obj/Debug/net10.0/AlertOpsBackend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/AlertOpsBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlertOpsBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e000ce6d4ca7cbbfd422879e91e7b7be1b1ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da4c001d8576ce31c20b56028c337da95cece60")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlertOpsBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlertOpsBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
